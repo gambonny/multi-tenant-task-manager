535b94796a152f0979b8d102c306c668e2c49552
Jonny Gamba <gambonny@gmail.com>
2026-03-03 16:31:13 -0500

    add wrangler deployment to @app

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,5 +15,6 @@ export default defineConfig({
 		tsconfigPaths(),
 		tailwindcss(),
 		react(),
+		cloudflare(),
 	],
 });
